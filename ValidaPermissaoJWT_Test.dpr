27dd915fa6e8148b5d896825e2c0b2907e4b8f5e
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2024-05-05 09:26:44 -0300

    Teste positivo criação usuário

--- ValidaPermissaoJWT_Test.dpr
+++ ValidaPermissaoJWT_Test.dpr
@@ -28,7 +28,8 @@ uses
   ValidaJwt.Model.Dao.TokenUsuario.InMemory in 'src\model\dao\in-memory\ValidaJwt.Model.Dao.TokenUsuario.InMemory.pas',
   ValidaJwt.Model.Dao.Usuario.InMemory in 'src\model\dao\in-memory\ValidaJwt.Model.Dao.Usuario.InMemory.pas',
   ValidaJwt.Model.Entity.TokenUsuario in 'src\model\entity\ValidaJwt.Model.Entity.TokenUsuario.pas',
-  ValidaJwt.Model.Entity.Usuario in 'src\model\entity\ValidaJwt.Model.Entity.Usuario.pas';
+  ValidaJwt.Model.Entity.Usuario in 'src\model\entity\ValidaJwt.Model.Entity.Usuario.pas',
+  App in 'src\App.pas';
 
 { keep comment here to protect the following conditional from being removed by the IDE when adding a unit }
 {$IFNDEF TESTINSIGHT}
@@ -42,6 +43,11 @@ begin
 {$IFDEF TESTINSIGHT}
   TestInsight.DUnitX.RunRegisteredTests;
 {$ELSE}
+
+{$IFDEF MSWINDOWS}
+  IsConsole := False;
+  ReportMemoryLeaksOnShutdown := True;
+{$ENDIF}
   try
     //Check command line options, will exit if invalid
     TDUnitX.CheckCommandLine;
@@ -50,7 +56,7 @@ begin
     //Tell the runner to use RTTI to find Fixtures
     runner.UseRTTI := True;
     //When true, Assertions must be made during tests;
-    runner.FailsOnNoAsserts := False;
+    runner.FailsOnNoAsserts := True;
 
     //tell the runner how we will log things
     //Log to the console window if desired
--- src/test/model/ValidaJwt.Model.Usuario.Test.pas
+++ src/test/model/ValidaJwt.Model.Usuario.Test.pas
@@ -9,12 +9,38 @@ type
   [TestFixture]
   ValidaJwtModelUsuarioTest = class
   public
+
+    [Test]
+    procedure Test_CriarUsuario;
   end;
 
 implementation
 
 uses
-  ValidaJwt.Model.Factory;
+  ValidaJwt.Model.Factory,
+  ValidaJwt.Dto.Req.ManutUsuario;
+
+{ ValidaJwtModelUsuarioTest }
+
+procedure ValidaJwtModelUsuarioTest.Test_CriarUsuario;
+begin
+  var LDto := TValidaJwtDtoReqCriarUsuario.Create;
+  LDto.Nome := 'Nome do Usuário';
+  LDto.Email := 'email@dominio.com';
+  LDto.Senha := 'senha-complexa';
+
+  var LUsuario := TValidaJwtModelFactory.New
+    .Usuario
+    .CriarUsuario(LDto);
+
+  Assert.IsTrue(Assigned(LUsuario), 'Usuário não cadastrado');
+  Assert.IsTrue(LUsuario.Id >0, 'Não foi gerado ID para o usuário');
+  Assert.AreEqual(LDto.Nome, LUsuario.Nome);
+  Assert.AreEqual(LDto.Email, LUsuario.Email);
+
+  LDto.Free;
+  LUsuario.Free;
+end;
 
 initialization
   TDUnitX.RegisterTestFixture(ValidaJwtModelUsuarioTest);
