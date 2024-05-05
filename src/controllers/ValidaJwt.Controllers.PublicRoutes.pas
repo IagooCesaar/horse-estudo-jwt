cd0b8005d2544504bb4cab12ecb37a4595d51321
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2024-05-05 09:02:45 -0300

    Rota para criar usuário

--- src/controllers/ValidaJwt.Controllers.PublicRoutes.pas
+++ src/controllers/ValidaJwt.Controllers.PublicRoutes.pas
@@ -12,11 +12,44 @@ procedure Registry(const AContext: string);
 implementation
 
 uses
-  ValidaJwt.Model.Factory;
+  ValidaJwt.Model.Factory,
+  ValidaJwt.Dto.Req.ManutUsuario;
+
+const
+  C_UNIT_NAME: string = 'ValidaJwt.Controllers.PublicRoutes';
+
+procedure PostCriarUsuario(Req: THorseRequest; Resp: THorseResponse);
+begin
+  if Req.Body = ''
+  then raise EHorseException.New
+    .Status(THTTPStatus.BadRequest)
+    .&Unit(C_UNIT_NAME)
+    .Error('O body não estava no formato esperado');
+
+  var LDto := TJson.ClearJsonAndConvertToObject
+    <TValidaJwtDtoReqCriarUsuario>(Req.Body);
+  try
+    var LTipoSoli := TValidaJwtModelFactory.New
+      .Usuario
+      .CriarUsuario(LDto);
+
+    Resp.Status(THTTPStatus.Created).Send(TJson.ObjectToClearJsonValue(LTipoSoli));
+
+    LTipoSoli.Free;
+  finally
+    LDto.Free;
+  end;
+end;
 
 procedure Registry(const AContext: string);
 begin
+  var LGroup := '/usuarios';
+  var LTag := 'Usuários';
 
+  THorse
+    .Group.Prefix(AContext + LGroup)
+    .Post('/', PostCriarUsuario)
+  ;
 end;
 
 end.
