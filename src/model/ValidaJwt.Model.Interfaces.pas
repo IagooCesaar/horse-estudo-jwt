a84f43e6f9e38399049ea0374a0c4b3028e152fe
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2024-05-05 08:39:40 -0300

    Preparando metodo Criar Usuario na Model

--- src/model/ValidaJwt.Model.Interfaces.pas
+++ src/model/ValidaJwt.Model.Interfaces.pas
@@ -2,9 +2,14 @@ unit ValidaJwt.Model.Interfaces;
 
 interface
 
+uses
+  ValidaJwt.Dto.Req.ManutUsuario,
+  ValidaJwt.Dto.Resp.Perfil;
+
 type
   IValidaJwtModelUsuario = interface
     ['{D34FABE8-C65D-4C62-A265-FEF97E427DC9}']
+    function CriarUsuario(ADto: TValidaJwtDtoReqCriarUsuario): TValidaJwtDtoRespPerfil;
   end;
 
   IValidaJwtModelFactory = interface
--- src/model/ValidaJwt.Model.Usuario.pas
+++ src/model/ValidaJwt.Model.Usuario.pas
@@ -3,15 +3,23 @@ unit ValidaJwt.Model.Usuario;
 interface
 
 uses
-  ValidaJwt.Model.Interfaces;
+  ValidaJwt.Model.Interfaces,
+
+  ValidaJwt.Model.Entity.Usuario,
+  ValidaJwt.Dto.Req.ManutUsuario,
+  ValidaJwt.Dto.Resp.Perfil;
 
 type
   TValidaJwtModelUsuario = class(TInterfacedObject,
     IValidaJwtModelUsuario)
+  private
+    function Mapper(ASource: TValidaJwtModelEntityUsuario): TValidaJwtDtoRespPerfil; overload;
+    function Mapper(ALista: TValidaJwtModelEntityUsuarioLista): TValidaJwtDtoRespPerfilLista; overload;
   public
     class function New: IValidaJwtModelUsuario;
 
     { IValidaJwtModelUsuario }
+    function CriarUsuario(ADto: TValidaJwtDtoReqCriarUsuario): TValidaJwtDtoRespPerfil;
   end;
 
 implementation
@@ -20,6 +28,31 @@ uses
   ValidaJwt.Model.Factory;
 
 { TValidaJwtModelUsuario }
+
+function TValidaJwtModelUsuario.CriarUsuario(ADto: TValidaJwtDtoReqCriarUsuario): TValidaJwtDtoRespPerfil;
+begin
+
+end;
+
+function TValidaJwtModelUsuario.Mapper(
+  ALista: TValidaJwtModelEntityUsuarioLista
+): TValidaJwtDtoRespPerfilLista;
+begin
+  Result := TValidaJwtDtoRespPerfilLista.Create;
+  for var LUsuario in ALista
+  do Result.Add(Mapper(LUsuario));
+end;
+
+function TValidaJwtModelUsuario.Mapper(
+  ASource: TValidaJwtModelEntityUsuario
+): TValidaJwtDtoRespPerfil;
+begin
+  Result := TValidaJwtDtoRespPerfil.Create;
+  Result.Id := ASource.Id;
+  Result.Nome := ASource.Nome;
+  Result.Email := ASource.Email;
+  Result.DataCriacao := ASource.DataCriacao;
+end;
 
 class function TValidaJwtModelUsuario.New: IValidaJwtModelUsuario;
 begin
