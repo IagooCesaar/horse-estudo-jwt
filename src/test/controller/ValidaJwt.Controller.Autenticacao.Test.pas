c74943814f40ceb017aad7f3919b70eef21d65f8
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2024-05-05 14:01:05 -0300

    Iniciando testes de geração de token

---
+++ src/test/controller/ValidaJwt.Controller.Autenticacao.Test.pas
@@ -0,0 +1,19 @@
+unit ValidaJwt.Controller.Autenticacao.Test;
+
+interface
+
+uses
+  DUnitX.TestFramework;
+
+type
+  [TestFixture]
+  TValidaJwtControllerAutenticacaoTest = class
+  public
+  end;
+
+implementation
+
+initialization
+  TDUnitX.RegisterTestFixture(TValidaJwtControllerAutenticacaoTest);
+
+end.
