f986bccb94544328a1c3fff2e125adf405790f64
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2024-05-05 07:04:51 -0300

    Iniciando o Horse

--- src/App.pas
+++ src/App.pas
@@ -27,37 +27,66 @@ end;
 
 implementation
 
+uses
+  System.SysUtils,
+  Horse,
+  Horse.Jhonson,
+  Horse.HandleException,
+  Horse.Compression;
+
 { TApp }
 
 constructor TApp.Create;
 begin
+  {$IFDEF MSWINDOWS}
+  IsConsole := False;
+  ReportMemoryLeaksOnShutdown := True;
+  {$ENDIF}
 
+  THorse.MaxConnections := StrToIntDef(GetEnvironmentVariable('MAXCONNECTIONS'), 10000);
+  THorse.ListenQueue := StrToIntDef(GetEnvironmentVariable('LISTENQUEUE'), 200);
+
+  THorse
+    .Use(Compression())
+    .Use(Jhonson('UTF-8'))
+    //HandleException deverá ser o último Middleware
+    .Use(HandleException);
 end;
 
 destructor TApp.Destroy;
 begin
-
+  while THorse.IsRunning do begin
+    THorse.StopListen;
+    Sleep(1000);
+  end;
   inherited;
 end;
 
 function TApp.GetBaseURL: string;
 begin
-
+  Result := Format('http://localhost:%d%s/%s', [THorse.Port, FContext, 'api']);
 end;
 
 function TApp.GetEmExecucao: Boolean;
 begin
-
+  Result := THorse.IsRunning;
 end;
 
 procedure TApp.Start(APort: Integer);
 begin
-
+  THorse.Listen(APort,
+    procedure begin
+      {$IF defined(CONSOLE) and (not defined(TEST))}
+      Writeln(Format('Server is runing on %s:%d', [THorse.Host, THorse.Port]));
+      Readln;
+      {$ENDIF}
+    end);
 end;
 
 procedure TApp.Stop;
 begin
-
+  while THorse.IsRunning
+  do THorse.StopListen;
 end;
 
 end.
